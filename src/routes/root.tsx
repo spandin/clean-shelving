e7253e6e1fc727fa73aa6b9258e3be0decd2d24a
Stanislau Pandzin <willstesi@gmail.com>
2023-12-05 12:27:23 +0300

    add active icons navbar

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,38 +2,77 @@ import "../scss/reset.scss";
 import "./_global.scss";
 import "./_root.scss";
 
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 import {
   BsHouse,
+  BsHouseFill,
   BsPieChart,
+  BsPieChartFill,
   BsPlusCircle,
-  BsSpeedometer2,
+  BsPlusCircleFill,
+  BsSpeedometer,
 } from "react-icons/bs";
 
 export default function Root() {
+  const location = useLocation();
+
   return (
     <div className="layout">
       <nav className="navbar">
         <Link className="navbar__avatar" to={"user/"}>
           S
         </Link>
+
         <div className="navbar__menu">
-          <Link className="navbar__menu__item" id="menu_add" to={"add/"}>
-            <BsPlusCircle />
-          </Link>
-          <Link className="navbar__menu__item" to={"products/"}>
-            <BsHouse />
-          </Link>
-          <Link className="navbar__menu__item" to={"statistics/"}>
-            <BsPieChart />
-          </Link>
           <Link
-            className="navbar__menu__item"
+            className={`navbar__menu__item ${
+              location.pathname === "/add/" ? "active" : null
+            }`}
+            id="menu_add"
+            to={"add/"}
+          >
+            {location.pathname === "/add/" ? (
+              <BsPlusCircle />
+            ) : (
+              <BsPlusCircleFill />
+            )}
+          </Link>
+
+          <Link
+            className={`navbar__menu__item ${
+              location.pathname === "/products/" ? "active" : null
+            }`}
+            to={"products/"}
+          >
+            {location.pathname === "/products/" ? <BsHouse /> : <BsHouseFill />}
+          </Link>
+
+          <Link
+            className={`navbar__menu__item ${
+              location.pathname === "/statistics/" ? "active" : null
+            }`}
+            to={"statistics/"}
+          >
+            {location.pathname === "/statistics/" ? (
+              <BsPieChart />
+            ) : (
+              <BsPieChartFill />
+            )}
+          </Link>
+
+          <Link
+            className={`navbar__menu__item ${
+              location.pathname === "/activity/" ? "active" : null
+            }`}
             id="menu_activity"
             to={"activity/"}
           >
-            <BsSpeedometer2 />
+            {location.pathname === "/activity/" ? (
+              <BsSpeedometer />
+            ) : (
+              <BsSpeedometer />
+            )}
           </Link>
         </div>
       </nav>
