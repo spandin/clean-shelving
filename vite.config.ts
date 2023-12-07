bc630e88a5bf8324a23ae99a52a71a75be1a88c9
Stanislau Pandzin <willstesi@gmail.com>
2023-12-07 22:55:35 +0300

    working on the configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: 'prompt',
@@ -41,5 +43,12 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
   plugins: [react(), VitePWA(manifestForPlugin)],
 })
