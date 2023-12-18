f4ef3442e3ebb92a065d7f0e10a9ece4330658ab
Stanislau Pandzin <willstesi@gmail.com>
2023-12-18 15:06:36 +0300

    Update product-header.tsx

--- src/routes/products-id/components/product-header.tsx
+++ src/routes/products-id/components/product-header.tsx
@@ -1,11 +1,10 @@
-import { ProductType } from "@/types/types";
-
 import { BsPencilSquare, BsTrash3 } from "react-icons/bs";
 
 import Informer from "@/components/common/informer/informer";
+import { DocumentData } from "firebase/firestore";
 
 interface IProps {
-  product: ProductType;
+  product: DocumentData;
 }
 
 export default function ProductHeader({ product }: IProps) {
