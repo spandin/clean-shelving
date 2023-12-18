1bc309adbbff19582e34ac41ac1805992f6797b8
Stanislau Pandzin <willstesi@gmail.com>
2023-12-18 14:40:31 +0300

    create getDocument func

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -1,4 +1,7 @@
-import { collection } from "firebase/firestore";
+import { DocumentData, collection, doc, getDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
-export const dataCollection = collection(db, 'data')
+export const dataCollection = collection(db, "data");
+export const getDocument = (productId: string): DocumentData => {
+  return getDoc(doc(db, `data/${productId}`));
+};
