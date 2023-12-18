5d91f97657e6ab4f27208296f2b250ae57f45d18
Stanislau Pandzin <willstesi@gmail.com>
2023-12-18 14:13:08 +0300

    Update settings.tsx

--- src/routes/settings/settings.tsx
+++ src/routes/settings/settings.tsx
@@ -1,12 +1,27 @@
 import "./_settings.scss";
 
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { removeUser } from "@/store/slices/userSlice";
+
 import Informer from "@/components/common/informer/informer";
 
 export default function Settings() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   return (
     <div className="settings">
       <div className="settings__header">
         <Informer title="Настройки" />
+      </div>
+      <div className="settings__footer">
+        <button
+          onClick={() => {
+            dispatch(removeUser()), navigate("/user/");
+          }}
+        >
+          Выйти
+        </button>
       </div>
     </div>
   );
