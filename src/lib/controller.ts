e6906bce82a8ab6f3b50912a6b2214ea0df34ec3
Stanislau Pandzin <willstesi@gmail.com>
2023-12-18 14:48:54 +0300

    Update controller.ts

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -2,6 +2,6 @@ import { DocumentData, collection, doc, getDoc } from "firebase/firestore";
 import { db } from "./firebase";
 
 export const dataCollection = collection(db, "data");
-export const getDocument = (productId: string): DocumentData => {
+export const getDocument = async (productId: string): Promise<DocumentData> => {
   return getDoc(doc(db, `data/${productId}`));
 };
