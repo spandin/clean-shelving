7aa7b3b7f32afd33e72d2c9c2e1ad316c58512da
Stanislau Pandzin <willstesi@gmail.com>
2023-12-19 14:01:56 +0300

    Update product-body.tsx

--- src/routes/products-id/components/product-body.tsx
+++ src/routes/products-id/components/product-body.tsx
@@ -1,48 +1,13 @@
 import { ProductType } from "@/types/types";
-import { DocumentData } from "firebase/firestore";
 
 import { convetTimestampToString } from "@/lib/date";
 
 interface IProps {
-  product: ProductType | DocumentData;
+  product: ProductType;
 }
 
 export default function ProductBody({ product }: IProps) {
   return (
-    <div className="product-id__wrapper__body">
-      <div className="product-id__wrapper__body__content">
-        <h1> Основная информация:</h1>
-        <div>
-          <span id="content_headline">Наименование: </span>
-          {product.name}
-        </div>
-        <div>
-          <span id="content_headline">Штрихкод: </span> {product.code}
-        </div>
-        <div>
-          <span id="content_headline">Категория: </span> {product.category}
-        </div>
-        <div>
-          <span id="content_headline">Статус: </span>
-          {product.actions.exported.isExported ? "Внесен" : "Не внесён"}
-        </div>
-      </div>
-
-      <div className="product-id__wrapper__body__content">
-        <h1>Даты:</h1>
-        <div>
-          <span id="content_headline">Добавлен: </span>{" "}
-          {convetTimestampToString(product.dates.createdAt)}
-        </div>
-        <div>
-          <span id="content_headline">Изготовлен: </span>{" "}
-          {convetTimestampToString(product.dates.mfd)}
-        </div>
-        <div>
-          <span id="content_headline">Просрочится: </span>
-          {convetTimestampToString(product.dates.exp)}
-        </div>
-      </div>
-    </div>
+    
   );
 }
