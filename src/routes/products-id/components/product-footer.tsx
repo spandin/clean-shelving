4c1e884c6343b014a73a9edb32a0da923e703277
Stanislau Pandzin <willstesi@gmail.com>
2023-12-19 14:02:27 +0300

    Update product-footer.tsx

--- src/routes/products-id/components/product-footer.tsx
+++ src/routes/products-id/components/product-footer.tsx
@@ -2,6 +2,13 @@ import { BsPencilSquare, BsTrash3 } from "react-icons/bs";
 
 export default function ProductFooter() {
   return (
-
+    <div className="product-id__footer">
+      <button className="circle_button">
+        <BsPencilSquare />
+      </button>
+      <button className="circle_button">
+        <BsTrash3 />
+      </button>
+    </div>
   );
 }
