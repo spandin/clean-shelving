8cab16ed0af8bbb5f3f214b379b1813f32a052cb
Stanislau Pandzin <willstesi@gmail.com>
2023-12-19 14:01:50 +0300

    Update product-id.tsx

--- src/routes/products-id/product-id.tsx
+++ src/routes/products-id/product-id.tsx
@@ -1,16 +1,17 @@
 import "./_product-id.scss";
+import { ProductType } from "@/types/types";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { db } from "@/lib/firebase";
 import { DocumentData, doc, getDoc } from "firebase/firestore";
 
-import { Ring } from "@uiball/loaders";
+import { convetTimestampToString } from "@/lib/date";
 
-import ProductHeader from "./components/product-header";
-import ProductBody from "./components/product-body";
-import ProductFooter from "./components/product-footer";
-import { ProductType } from "@/types/types";
+import { Ring } from "@uiball/loaders";
+import { BsPencilSquare, BsTrash3 } from "react-icons/bs";
+
+import Informer from "@/components/common/informer/informer";
 
 export default function ProductId() {
   const theme = window.matchMedia("(prefers-color-scheme: dark)");
@@ -19,17 +20,13 @@ export default function ProductId() {
   const [product, setProduct] = useState<ProductType | DocumentData>();
 
   useEffect(() => {
-    const fetchProductData = async (): Promise<void> => {
-      try {
-        const productSnap = await getDoc(doc(db, "data", `${productId}`));
+    const fetchProductData = async () => {
+      const productSnap = await getDoc(doc(db, "data", `${productId}`));
 
-        if (productSnap.exists()) {
-          console.log(productSnap.data());
-
-          setProduct(productSnap.data());
-        }
-      } catch (error) {
-        console.log(`ProductID: ${error}`);
+      if (productSnap.exists()) {
+        setProduct(productSnap.data());
+      } else {
+        console.log(`No such doc`);
       }
     };
 
@@ -43,10 +40,65 @@ export default function ProductId() {
       {product ? (
         <>
           <div className="product-id__wrapper">
-            <ProductHeader product={product} />
-            <ProductBody product={product} />
+            <div className="product-id__wrapper__header">
+              <Informer title={`${product.name}`} subtitle={product.category} />
+
+              <div className="product-id__wrapper__header__buttons">
+                <button className="circle_button">
+                  <BsPencilSquare />
+                </button>
+                <button className="circle_button">
+                  <BsTrash3 />
+                </button>
+              </div>
+            </div>
+
+            <div className="product-id__wrapper__body">
+              <div className="product-id__wrapper__body__content">
+                <h1> Основная информация:</h1>
+                <div>
+                  <span id="content_headline">Наименование: </span>
+                  {product.name}
+                </div>
+                <div>
+                  <span id="content_headline">Штрихкод: </span> {product.code}
+                </div>
+                <div>
+                  <span id="content_headline">Категория: </span>{" "}
+                  {product.category}
+                </div>
+                <div>
+                  <span id="content_headline">Статус: </span>
+                  {product.actions.exported.isExported ? "Внесен" : "Не внесён"}
+                </div>
+              </div>
+
+              <div className="product-id__wrapper__body__content">
+                <h1>Даты:</h1>
+                <div>
+                  <span id="content_headline">Добавлен: </span>{" "}
+                  {convetTimestampToString(product.dates.createdAt)}
+                </div>
+                <div>
+                  <span id="content_headline">Изготовлен: </span>{" "}
+                  {convetTimestampToString(product.dates.mfd)}
+                </div>
+                <div>
+                  <span id="content_headline">Просрочится: </span>
+                  {convetTimestampToString(product.dates.exp)}
+                </div>
+              </div>
+            </div>
           </div>
-          <ProductFooter />
+
+          <div className="product-id__footer">
+            <button className="circle_button">
+              <BsPencilSquare />
+            </button>
+            <button className="circle_button">
+              <BsTrash3 />
+            </button>
+          </div>
         </>
       ) : (
         <Ring size={30} color={theme.matches ? "#ffffff" : "#121212"} />
