50db2909e62a54331fb0537a6a46697a8e284df7
Stanislau Pandzin <willstesi@gmail.com>
2023-12-19 14:02:25 +0300

    Update product-header.tsx

--- src/routes/products-id/components/product-header.tsx
+++ src/routes/products-id/components/product-header.tsx
@@ -11,6 +11,17 @@ interface IProps {
 
 export default function ProductHeader({ product }: IProps) {
   return (
+    <div className="product-id__wrapper__header">
+      <Informer title={`${product.name}`} subtitle={product.category} />
 
+      <div className="product-id__wrapper__header__buttons">
+        <button className="circle_button">
+          <BsPencilSquare />
+        </button>
+        <button className="circle_button">
+          <BsTrash3 />
+        </button>
+      </div>
+    </div>
   );
 }
