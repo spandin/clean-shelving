b45ed19e5b71937baeb4e3b914babb126e2d101e
Stanislau Pandzin <willstesi@gmail.com>
2023-12-20 11:42:19 +0300

    Update products-header.tsx

--- src/routes/products/components/products-header.tsx
+++ src/routes/products/components/products-header.tsx
@@ -1,12 +1,21 @@
-import Informer from "@/components/common/informer/informer";
+import { useNavigate } from "react-router-dom";
+
 import { BsFilterCircle, BsPlusCircle, BsSearch } from "react-icons/bs";
 
-export default function ProductsHeader() {
+import Informer from "@/components/common/informer/informer";
+
+export default function ProductsHeader({ props }: { props: number }) {
+  const navigate = useNavigate();
+
   return (
     <div className="products__header">
-      <Informer title="Категория" subtitle="Кол. позиций" />
+      <Informer title="Категория" subtitle={`${props} позиции`} />
       <div className="products__header__buttons">
-        <button className="circle_button" id="add">
+        <button
+          className="circle_button"
+          id="add"
+          onClick={() => navigate("/add/")}
+        >
           <BsPlusCircle />
         </button>
         <button className="circle_button" id="filter">
