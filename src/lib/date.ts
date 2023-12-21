55dedeca479705ba1a166e2dfb62e4275ffeb879
Stanislau Pandzin <willstesi@gmail.com>
2023-12-21 23:00:33 +0300

    add new func

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,14 +1,31 @@
-import { addMonths, getTime, toDate } from "date-fns";
+import { addMonths, getTime, intlFormatDistance, toDate } from "date-fns";
 
 export const timestampToString = (timestamp: number): string => {
   return toDate(timestamp).toLocaleDateString("ru-Ru");
 };
 
-export const stringToUTC = (dateStr: string): number => {
+export const stringToTimestamp = (dateStr: string): number => {
   const pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
+
   return getTime(new Date(dateStr?.replace(pattern, "$3-$2-$1")));
 };
 
-export const calcEndDate = (mfd: string, exp: number): Date => {
-  return addMonths(new Date(mfd), exp);
+export const stringToUTC = (dateStr: string): Date => {
+  const pattern = /(\d{2})\.(\d{2})\.(\d{4})/;
+
+  return new Date(dateStr?.replace(pattern, "$3-$2-$1"));
+};
+
+export const calcDistanceEndFromExp = (exp: string): string => {
+  return intlFormatDistance(stringToUTC(exp), new Date());
+};
+
+export const calcDistanceEndFromMonth = (
+  mfd: string,
+  month: string
+): string => {
+  return intlFormatDistance(
+    addMonths(stringToUTC(mfd), parseInt(month)),
+    new Date()
+  );
 };
