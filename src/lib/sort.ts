9ef3139c8b48ce284db938fd04f9242893e3be11
Stanislau Pandzin <willstesi@gmail.com>
2023-12-28 15:19:29 +0300

    added sorting functions

---
+++ src/lib/sort.ts
@@ -0,0 +1,20 @@
+import { ProductType } from "@/types/types";
+
+export const isExported = (array: ProductType[]): ProductType[] =>
+  array.filter((product) => product.actions.exported.isExported === true);
+
+export const isNotExported = (array: ProductType[]): ProductType[] =>
+  array.filter((product) => product.actions.exported.isExported === false);
+
+export const findInArrayBy = (
+  array: ProductType[],
+  by: string
+): ProductType[] =>
+  array.filter((product) =>
+    Object.values(product).some((value) => ("" + value).indexOf(by) !== -1)
+  );
+
+export const realLenghtArr = (array: ProductType[]): number =>
+  array.filter(function () {
+    return true;
+  }).length;
