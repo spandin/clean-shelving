ce512a9823fd273588fc5020607ecab5e2a9eb2f
Stanislau Pandzin <willstesi@gmail.com>
2024-01-02 21:06:27 +0300

    refactoring

--- src/components/common/export-button/export-button.tsx
+++ src/components/common/export-button/export-button.tsx
@@ -1,53 +1,25 @@
 import "./_export-button.scss";
-
-import * as FileSaver from "file-saver";
-import * as XLSX from "xlsx";
-
-import { useAppSelector } from "@/hooks/redux-hooks";
-
-import { timestampToString } from "@/lib/date";
+import { useState } from "react";
 
 import { BsDatabaseDown } from "react-icons/bs";
-import { FilteredProducts } from "@/routes/products/filtered-products";
 
-export default function ExportButton({ fileName }: { fileName: string }) {
-  const fileType =
-    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
-  const fileExtension = ".xlsx";
+import { Modal } from "../modal/modal";
+import { ExportProduct } from "@/components/products/export-product/export-product";
 
-  const exportToCSV = (apiData: any[], fileName: string) => {
-    const ws = XLSX.utils.json_to_sheet(apiData);
-    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
-    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-    const data = new Blob([excelBuffer], { type: fileType });
-    FileSaver.saveAs(data, fileName + fileExtension);
-  };
-
-  const { category, exported } = useAppSelector((state) => state.data.filter);
-  const products = useAppSelector((state) => state.data.products);
-
-  const dataForExport =
-    products &&
-    FilteredProducts(products, category, exported).map(
-      ({ name, code, quantity, dates, actions }) => ({
-        Имя: name,
-        Штрихкод: code,
-        Количество: quantity,
-        Создан: dates.createdAt
-          ? timestampToString(dates.createdAt)
-          : timestampToString(actions.updated.updatedAt),
-        Изготовлен: timestampToString(dates.mfd),
-        Просрочится: timestampToString(dates.mfd),
-        Статус: actions.exported.isExported ? "Внесён" : "Не внесён",
-      })
-    );
-
+export default function ExportButton() {
+  const [exportModalActive, setExportModalActive] = useState(false);
   return (
-    <button
-      className="export_button"
-      onClick={() => exportToCSV(dataForExport, fileName)}
-    >
-      <BsDatabaseDown />
-    </button>
+    <>
+      <button
+        className="export_button"
+        onClick={() => setExportModalActive(true)}
+      >
+        <BsDatabaseDown />
+      </button>
+
+      <Modal active={exportModalActive} setActive={setExportModalActive}>
+        <ExportProduct />
+      </Modal>
+    </>
   );
 }
