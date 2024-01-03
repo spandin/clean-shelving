1e2193639fe2e601c2c64b320436797527699d72
Stanislau Pandzin <willstesi@gmail.com>
2024-01-04 02:02:45 +0300

    refactoring

--- src/routes/user/user.tsx
+++ src/routes/user/user.tsx
@@ -1,33 +1,40 @@
 import "./_user.scss";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { useAuth } from "@/hooks/use-auth";
+import { useAppDispatch, useAppSelector } from "@/hooks/redux-hooks";
 
 import { BsGear } from "react-icons/bs";
 
 import Informer from "@/components/common/informer/informer";
 import Login from "@/components/auth/login";
 
+import IMAGES from "@/assets/images";
+import { getUserInfo } from "@/store/slices/userSlice";
+
 export default function User() {
-  const { isAuth, email } = useAuth();
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  const user = useAppSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getUserInfo(`${user.id}`));
+  }, [dispatch, user.id]);
 
   return (
     <div className="user">
-      {isAuth ? (
+      {user.isAuth ? (
         <>
           <div className="user__wrapper">
             <div className="user__wrapper__header">
-              <Informer title="Имя" subtitle={email} />
-
-              <div className="user__wrapper__header__buttons">
-                <button
-                  className="circle_button"
-                  onClick={() => navigate("/settings/")}
-                >
-                  <BsGear />
-                </button>
-              </div>
+              <Informer title="Имя" subtitle={user.email} />
+              <button
+                className="circle_button"
+                onClick={() => navigate("/settings/")}
+              >
+                <BsGear />
+              </button>
             </div>
 
             <div className="user__wrapper__body">
@@ -36,11 +43,29 @@ export default function User() {
                   <input type="radio" name="tabs" id="tab-1" defaultChecked />
                   <label htmlFor="tab-1">Информация</label>
                   <div className="tabs__tab__content">
-                    <img
-                      id="user-tabs-avatar"
-                      src="https://img.goodfon.com/original/1280x960/6/51/victoria-berngard-utro-leto.jpg"
-                    />
-                    <h2>Имя пользователя</h2>
+                    <img id="user_avatar" src={IMAGES.user_avatar} />
+
+                    <div className="tabs__tab__content__user-info">
+                      <h2>{user.name}</h2>
+                      <p>{user.email}</p>
+                    </div>
+
+                    <div className="tabs__tab__content__user-actions">
+                      <div className="tabs__tab__content__user-actions__col">
+                        <span>Добавил</span>
+                        <span>{user.actions && user.actions.added}</span>
+                      </div>
+
+                      <div className="tabs__tab__content__user-actions__col">
+                        <span>Обновил</span>
+                        <span>{user.actions && user.actions.updated}</span>
+                      </div>
+
+                      <div className="tabs__tab__content__user-actions__col">
+                        <span>Удалил</span>
+                        <span>{user.actions && user.actions.deleted}</span>
+                      </div>
+                    </div>
                   </div>
                 </li>
                 <li className="tabs__tab">
