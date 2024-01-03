dd7c0b597887569b29fc78fbcf8f6bbd9050d974
Stanislau Pandzin <willstesi@gmail.com>
2024-01-04 02:03:28 +0300

    add auth function in reducer

--- src/store/slices/userSlice.ts
+++ src/store/slices/userSlice.ts
@@ -1,32 +1,73 @@
 import { UserData } from "@/types/types";
-import { createSlice } from "@reduxjs/toolkit";
+
+import { auth, db } from "@/lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const signInUser = createAsyncThunk(
+  "@@user/signInUser",
+  async (data: { email: string; password: string }) => {
+    try {
+      return signInWithEmailAndPassword(auth, data.email, data.password).then(
+        (userCredential) => {
+          return userCredential.user;
+        }
+      );
+    } catch (e: any) {
+      console.log(`SIGN IN`, e.message);
+    }
+  }
+);
+
+export const getUserInfo = createAsyncThunk(
+  "@@user/getUserInfo",
+  async (id: string) => {
+    const docSnap = await getDoc(doc(db, "users", `${id}`));
+
+    return docSnap.data();
+  }
+);
 
 const initialState: UserData = {
-  email: null,
-  token: null,
   id: null,
+  name: "Гость",
+  email: null,
   isAuth: false,
+  actions: {
+    added: 0,
+    updated: 0,
+    deleted: 0,
+  },
+  activity: [],
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser(state, action) {
-      state.email = action.payload.email;
-      state.token = action.payload.token;
-      state.id = action.payload.id;
-      state.isAuth = true;
-    },
     removeUser(state) {
       state.email = null;
-      state.token = null;
       state.id = null;
       state.isAuth = false;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(signInUser.fulfilled, (state, action) => {
+        state.email = action.payload ? action.payload.email : null;
+        state.id = action.payload ? action.payload.uid : null;
+        state.isAuth = true;
+      })
+      .addCase(getUserInfo.fulfilled, (state, action) => {
+        state.name = action.payload?.name;
+        state.actions = action.payload?.actions;
+        state.activity = action.payload?.activity;
+      });
+  },
 });
 
-export const { setUser, removeUser } = userSlice.actions;
+export const { removeUser } = userSlice.actions;
 
 export default userSlice.reducer;
