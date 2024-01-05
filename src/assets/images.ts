a166a078a16ee6d6a7fdeb45292b48ab8b42375e
Stanislau Pandzin <willstesi@gmail.com>
2024-01-05 16:57:39 +0300

    add images

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,5 +1,6 @@
 const IMAGES = {
-  welcoming: new URL("./welcoming.svg", import.meta.url).href,
+  login: new URL("./login.svg", import.meta.url).href,
+  registration: new URL("./registration.svg", import.meta.url).href,
   user_avatar: new URL("./user_avatar.svg", import.meta.url).href,
 };
 
