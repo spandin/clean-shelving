0a12ec16570829c577146fdf31e07e31f04e413f
Stanislau Pandzin <willstesi@gmail.com>
2024-02-20 11:40:30 +0300

    Update modal.tsx

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -9,7 +9,19 @@ export const Modal = ({
   setActive: React.Dispatch<React.SetStateAction<boolean>>;
   children: React.ReactNode;
 }) => {
-  return (
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent) ? (
+    <div
+      className={active ? "modal active" : "modal"}
+      onTouchStart={() => setActive(false)}
+    >
+      <div
+        className={active ? "modal__content active" : "modal__content"}
+        onTouchStart={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>
+  ) : (
     <div
       className={active ? "modal active" : "modal"}
       onClick={() => setActive(false)}
