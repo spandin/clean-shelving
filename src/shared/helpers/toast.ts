e9f39a8af42c85719b16fcc439f518d239e18a3a
Stanislau Pandzin <willstesi@gmail.com>
2024-02-20 15:54:11 +0300

    working with toast style

--- src/shared/helpers/toast.ts
+++ src/shared/helpers/toast.ts
@@ -1,7 +1,7 @@
 import { ToastOptions, toast } from "react-toastify";
 
 const settingsToast: ToastOptions = {
-  position: "bottom-center",
+  position: "top-center",
   autoClose: 1500,
   hideProgressBar: false,
   closeOnClick: true,
