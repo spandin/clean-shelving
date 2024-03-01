2392a9fe6a02daab70f82155bf5e054c19a73753
Stanislau Pandzin <willstesi@gmail.com>
2024-03-01 19:28:19 +0300

    Update settingsSlice.ts

--- src/app/slices/settingsSlice.ts
+++ src/app/slices/settingsSlice.ts
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { setThemeAndMeta } from "@/shared/helpers/theme";
+
 interface settingsState {
   selectType: string;
   theme: string;
@@ -19,24 +21,8 @@ const settingsSlice = createSlice({
     },
     setTheme: (state, action) => {
       state.theme = action.payload;
-      document.body.setAttribute("data-theme", action.payload);
 
-      const oldMetaTag =
-        document.querySelector('meta[name="theme-color"]') ||
-        document.createElement("meta");
-
-      const newMetaTag = document.createElement("meta");
-      newMetaTag.setAttribute("name", "theme-color");
-
-      if (action.payload === "light") {
-        newMetaTag.content = "#f8f8f8";
-        document.head.replaceChild(newMetaTag, oldMetaTag);
-        console.log(1);
-      } else {
-        newMetaTag.content = "#202020";
-        document.head.replaceChild(newMetaTag, oldMetaTag);
-        console.log(2);
-      }
+      setThemeAndMeta(action.payload);
     },
   },
 });
