d633cdfd9ff52184837256ee9e320cc5aa865be4
Stanislau Pandzin <willstesi@gmail.com>
2024-03-10 14:02:28 +0300

    add export barcodes page

---
+++ src/pages/export-barcodes-page/export-barcodes.tsx
@@ -0,0 +1,13 @@
+import XLSX from "xlsx";
+
+import { useAppSelector } from "@/shared/hooks/use-redux";
+
+export const ExportDataToExcel = () => {
+  const data = useAppSelector((state) => state.data.barcodes); // Получаем данные из хранилища Redux
+
+  const ws = XLSX.utils.json_to_sheet(data);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+
+  return XLSX.writeFile(wb, "exported_data.xlsx");
+};
