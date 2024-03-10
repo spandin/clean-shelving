4fb3d400d164e1cb645dbccb3ff3fc4e7eed4b3f
Stanislau Pandzin <willstesi@gmail.com>
2024-03-10 15:36:13 +0300

    Update import-xslx.tsx

--- src/features/import-xslx/ui/import-xslx.tsx
+++ src/features/import-xslx/ui/import-xslx.tsx
@@ -1,12 +1,9 @@
+import * as XLSX from "xlsx";
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
-import * as XLSX from "xlsx";
 
 import { setDoc } from "firebase/firestore";
 import { query } from "@/shared/api/firebase-config";
-import { ProductType } from "@/shared/types/types";
-
-import { nanoid } from "@reduxjs/toolkit";
 
 export function ImportXSLX() {
   const [files, setFiles] = useState([]);
@@ -35,40 +32,11 @@ export function ImportXSLX() {
         const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
 
         rows.forEach(async (row: any) => {
-          const id = nanoid();
-          const productObject: ProductType = {
-            id: id,
-            name: row[0],
-            code: parseInt(row[1]),
+          await setDoc(query(`barcodes/${row[0]}`), {
+            code: row[0],
+            name: row[1],
             category: row[2],
-            quantity: row[3],
-            dates: {
-              createdAt: parseInt(row[4]),
-              mfd: parseInt(row[5]),
-              exp: parseInt(row[6]),
-            },
-            actions: {
-              created: {
-                createdAt: parseInt(row[6]),
-                whoCreated: row[7],
-                whoCreatedID: row[9],
-              },
-              exported: {
-                isExported: row[11],
-                exportedOn: parseInt(row[10]),
-                whoExported: row[12],
-                whoExportedID: row[13],
-              },
-              updated: {
-                isUpdated: row[14],
-                whoUpdated: row[16],
-                whoUpdatedID: row[17],
-                updatedAt: parseInt(row[15]),
-              },
-            },
-          };
-
-          await setDoc(query(`data/${id}`), productObject);
+          });
         });
       });
     };
