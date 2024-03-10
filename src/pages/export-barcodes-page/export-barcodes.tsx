0cb27fb53aa72312d94498c335fd3def6ce3e3ec
Stanislau Pandzin <willstesi@gmail.com>
2024-03-10 14:13:41 +0300

    Update export-barcodes.tsx

--- src/pages/export-barcodes-page/export-barcodes.tsx
+++ src/pages/export-barcodes-page/export-barcodes.tsx
@@ -1,19 +1,35 @@
-import XLSX from "xlsx";
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
 
 import { useAppSelector } from "@/shared/hooks/use-redux";
 
-export const ExportDataToExcel = () => {
-  const data = useAppSelector((state) => state.data.barcodes); // Получаем данные из хранилища Redux
+export default function ExportDataToExcel() {
+  const fileType =
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+  const fileExtension = ".xlsx";
 
-  const ws = XLSX.utils.json_to_sheet(data);
-  const wb = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+  const exportToCSV = (apiData: any[], fileName: string) => {
+    const ws = XLSX.utils.json_to_sheet(apiData);
+    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const data = new Blob([excelBuffer], { type: fileType });
+    FileSaver.saveAs(data, fileName + fileExtension);
+  };
+
+  const data = useAppSelector((state) => state.data.barcodes);
+
+  const exportData = data.map(({ name, code, category }) => ({
+    Штрихкод: code,
+    Имя: name,
+    Категория: category,
+  }));
 
   return (
     <div>
-      <button onClick={XLSX.writeFile(wb, "exported_data.xlsx")}>
-        Скачать БД ШК
-      </button>
+      <button
+        value={"Скачать"}
+        onClick={() => exportToCSV(exportData, "CS-Barcodes")}
+      />
     </div>
   );
-};
+}
